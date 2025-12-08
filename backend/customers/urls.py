4536cf800ee3320943efff1631b6c1a69685398f
Justin Boucher <justin.m.boucher@gmail.com>
2025-12-07 18:56:50 -0600

    Fix routing issue on customers and projects

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CustomerViewSet
 
 router = DefaultRouter()
-router.register(r"customers", CustomerViewSet, basename="customer")
+router.register(r"", CustomerViewSet, basename="customer")
 
 urlpatterns = [
     path("", include(router.urls)),
